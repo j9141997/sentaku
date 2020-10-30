9ee5bc9b2d37bccc0c9fd7857320172ba64d3c61
JunkiYoshida <junkiyoshida@JunkiYoshidanoMacBook-Air.local>
2020-10-30 14:58:37 +0900

    feat: add attribute

--- src/components/organism/ArticleForm.tsx
+++ src/components/organism/ArticleForm.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react'
 export const ArticleForm: FC = () => {
   return (
     <div>
-      <input type="text" name="" id="" />
+      <input type="text" name="title" />
       <button type="submit">投稿する</button>
     </div>
   )
